5b36996e58e7b378a107590455c5480bbbf1a8fc
cmkxak <gcm466@naver.com>
2025-07-02 00:17:53 +0900

    add: custom logger util

--- src/utils/CustomLogger.java
+++ src/utils/CustomLogger.java
@@ -1,2 +1,17 @@
-package utils;public class CustomLogger {
+package utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class CustomLogger {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+
+    public static void log(Object obj) {
+        String now = LocalDateTime.now().format(formatter);
+        System.out.printf("[%s] [%9s] %s\n", now, Thread.currentThread().getName(), obj);
+    }
+
+
+
 }
